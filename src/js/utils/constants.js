55bf7ad41a6cbbb1b267aca6e8617544b45b8d20
Kaidi Pei <kaidipei@uw.edu>
2019-06-05 15:18:19 -0700

    add to summary parser

--- src/js/utils/constants.js
+++ src/js/utils/constants.js
@@ -1,3 +1,8 @@
 export const DENSE = 'Dense';
 export const CONV_2D = 'Conv2D';
 export const MAX_POOLING_2D = 'MaxPooling2D';
+export const RECT_ATTR = '{"shape": "rect", \
+                            "color": "white", \
+                            "width": "150px", \
+                            "height": "50px", \
+                            "text": ["name"]}'
