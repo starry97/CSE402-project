04d78523fca0c4cee5012db4c24635f5429f8cd9
starry <starrynight0225@gmail.com>
2019-06-10 15:02:12 -0700

    fix

--- src/index.js
+++ src/index.js
@@ -15,13 +15,13 @@ document.getElementById('upload_file').addEventListener('change', (evt) => {
     downloadPNGButton.innerHTML = "Download as PNG";
     document.getElementById('download_buttons').appendChild(downloadSVGButton);
     document.getElementById('download_buttons').appendChild(downloadPNGButton);
+    document.getElementById('download_png').addEventListener('click', () => {
+      saveAsPNG(document.getElementById(SVG_ID));
+    });
+    
+    document.getElementById('download_svg').addEventListener('click', () => {
+      saveAsSVG(document.getElementById(SVG_ID));
+    });
   }
 });
 
-document.getElementById('download_png').addEventListener('click', () => {
-  saveAsPNG(document.getElementById(SVG_ID));
-});
-
-document.getElementById('download_svg').addEventListener('click', () => {
-  saveAsSVG(document.getElementById(SVG_ID));
-});
