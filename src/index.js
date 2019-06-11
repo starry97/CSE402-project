b097d08efc553be74967f33e4467a48796adb0e8
Kaidi Pei <kaidipei@uw.edu>
2019-06-10 18:20:25 -0700

    add debug session

--- src/index.js
+++ src/index.js
@@ -3,10 +3,11 @@ import { saveAsPNG } from './js/utils/saveAsPNG';
 import { SVG_ID, DEBUG } from './js/utils/constants';
 import { saveAsSVG } from './js/utils/saveAsSVG';
 import MViz from './js/components/MViz';
+import { DEBUG_JSON } from './js/utils/debugJson';
 
 // for debugging
 if (DEBUG) {
-  const json = '{"layers":[{"name":"embedding_1","type":"Embedding","subLayers":["embedding_1_1","embedding_1_2","embedding_1_3","embedding_1_4"]},{"name":"lstm_1","type":"LSTM","subLayers":["lstm_1_1","lstm_1_2","lstm_1_3","lstm_1_4"]},{"name":"dense_1","type":"Dense"}],"attributes":{"Embedding":{"shape":"rect","fill":"d3.rgb(255, 245, 236)","width":"150","height":"50","text":["type"]},"LSTM":{"shape":"rect","fill":{"r":253,"g":186,"b":134,"opacity":1},"width":"150","height":"50","text":["type"]},"Dense":{"shape":"rect","fill":{"r":219,"g":86,"b":12,"opacity":1},"width":"150","height":"50","text":["type"]}}}';
+  const json = DEBUG_JSON;
   const mv = new MViz(json);
   mv.draw();
 }
